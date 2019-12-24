4795dc6f0a05244bb7940c4b8fc0469688bfe284
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-24 12:43:43 +0530

    Updates

---
+++ precache-manifest.612bcb654873894e680935e92fb5cd4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a51a6bb0f68b40561bdf6c019e3243e",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f36658f570de4512fb65",
+    "url": "/react-todo-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "366f9263dd95f8dd7023",
+    "url": "/react-todo-app/static/js/2.89787e4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-app/static/js/2.89787e4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f36658f570de4512fb65",
+    "url": "/react-todo-app/static/js/main.d5941d9f.chunk.js"
+  },
+  {
+    "revision": "cc6ad9f6d96f5e014cca",
+    "url": "/react-todo-app/static/js/runtime-main.2ae795a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
